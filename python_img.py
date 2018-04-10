f5c74f8166d8b5076d25d5ff777e10ea9b921b0b
dyz330542613 <dyz330542613@gmail.com>
2018-04-10 20:08:31 +0800

    modify filepath

--- python_img.py
+++ python_img.py
@@ -9,29 +9,28 @@ from hbase import Hbase
 from hbase.ttypes import *
 
 #### base set
-find_path = (r'/image/', )
+find_path = '/var/www/html/Spark_SQL/img_store'
 
 
 class HbaseWrite():
     def __init__(self):
-        self.tableName = 'img_store'
-        self.transport = TSocket.TSocket('10.42.0.235', 9090)
+        self.tableName = 'img_store222'
+        self.transport = TSocket.TSocket('student62', 9090)
         self.transport = TTransport.TBufferedTransport(self.transport)
         self.transport.open()
         self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
         self.client = Hbase.Client(self.protocol)
 
-    def createTable(self, tableName):
+    def createTable(self):
         col1 = ColumnDescriptor(name="data:", maxVersions=1)
-        self.client.createTable(tableName, [col1])
+        self.client.createTable(self.tableName, [col1])
 
     def write(self, PicPath, PicName):
         row = PicName.split('.')[0]
         _data = PicName.split('.')[1]
         PicData = open('%s/%s' % (PicPath, PicName), 'rb').read()
-        # 此处需要注意格式，网上的格式报错，少个参数报错如下
         # TypeError: mutateRow() takes exactly 5 arguments (4 given)
-        self.client.mutateRow(self.tableName, row, [Mutation(column='data:%s' % _data, value=PicData)], {})
+        self.client.mutateRow(self.tableName, row, [Mutation(column='data:%s' % _data, value=PicData)])
 
     def read(self, tableName, PicName):
         row = PicName.split('.')[0]
@@ -47,8 +46,8 @@ def main(_path):
     WHB = HbaseWrite()
     WHB.createTable()
     find_file = re.compile(r'^[0-9a-z]*.jpg$')
-    # find_walk = os.walk(_path)
-    for path, dirs, files in '/var/www/html/spark_sql/img_store':
+    find_walk = os.walk(_path)
+    for path, dirs, files in find_walk:
         for f in files:
             if find_file.search(f):
                 path_name = path
@@ -57,5 +56,5 @@ def main(_path):
 
 
 if __name__ == '__main__':
-    for get_path in find_path:
-        main(get_path)
+    # for get_path in find_path:
+        main(find_path)
